9e5524522c316bb26e539abb8c96060e812f5a4f
Yang Li-Yi <joseph.lyyang@gmail.com>
2016-10-21 17:15:15 +0800

    Chapter1 Exercise1: Create debug.text when run rackup

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -3,6 +3,7 @@ require "rulers/version"
 module Rulers
   class Application
     def call(env)
+      `echo debug > debug.txt`;
       [200, {'Content-Type' => 'text/html'},
         ["Hello from Ruby on Rulers!"]]
     end
