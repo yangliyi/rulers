f2f198833aa2652a069903668625784743e7fc7f
Yang Li-Yi <joseph.lyyang@gmail.com>
2016-11-01 16:10:56 +0800

    Chapter2 Exercise3: Roots and Routes

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -7,6 +7,22 @@ module Rulers
       if env['PATH_INFO'] == '/favicon.ico'
         return [404, {'Content-Type' => 'text/html'}, []]
       end
+      # Chapter2 Exercise3
+      if env['PATH_INFO'] == '/'
+        # env['PATH_INFO'] = '/quotes/a_quote'
+        # Chapter2 Exercise3 Answer0
+
+        # Chapter2 Exercise3 Answer1
+        # return [200, {'Content-Type' => 'text/html'}, [File.read(File.expand_path("index.html"))]]
+
+        # Chapter2 Exercise3 Answer2
+        # controller = HomeController.new(env)
+        # text = controller.send("index")
+        # return [200, {'Content-Type' => 'text/html'}, [text]]
+
+        # Chapter2 Exercise3 Answer3
+        return ['302', { 'Content-Type' => 'text/html', 'Location' => "/quotes/a_quote" }, ["302 you've redirected"]]
+      end
 
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
@@ -28,4 +44,11 @@ module Rulers
       @env
     end
   end
+
+  # Chapter2 Exercise3 Answer2
+  # class HomeController < Controller
+  #   def index
+  #     "Home Page"
+  #   end
+  # end
 end
